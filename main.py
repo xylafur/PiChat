3f15118de2e52228ef6372e2c69738ff822dfd81
xylafur <jkesleyrichardson@gmail.com>
2016-10-29 17:06:17 -0500

    re routing after login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__)
 
@@ -6,6 +6,15 @@ app=Flask(__name__)
 def login():
     return render_template("index.html")
 
+@app.route('/', methods=['POST'])
+def afterLogin():
+    userName = request.form['userName']
+    print(url_for('avaliableRooms'))
+    return redirect(url_for('avaliableRooms'))
+
+@app.route('/rooms')
+def avaliableRooms():
+    return render_template("rooms.html")
 
 if __name__ == '__main__':
     app.run(debug= True, host='0.0.0.0')
