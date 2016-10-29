0e43e344dff650397674b5c9b3d2590ca33d2fd6
tetrovolt <raymond@sutrisno.me>
2016-10-29 17:05:30 -0500

    modified socket main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request
+#!/usr/bin/python3
+
+print "starting server..."
+
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 
 app=Flask(__name__)
-
 @app.route('/')
 def login():
     return render_template("index.html")
 
 
+
+
+
+
+
+#===========Main===========#
 if __name__ == '__main__':
     app.run(debug= True, host='0.0.0.0')
