ad9cd866e330d6dab1e7786166ef20a35a4b9569
xylafur <jkesleyrichardson@gmail.com>
2016-10-29 21:26:30 -0500

    fixing the funky

---
+++ usermanager.py
@@ -0,0 +1,13 @@
+import json
+
+def getUser(username,location):
+    name = username
+    print name
+    loc = location
+    print loc
+    data = {}
+    data['username'] = name
+    data['longitude'] = location[0]
+    data['latitude'] = location[1]
+    json_data = json.dumps(data)
+    return json_data
